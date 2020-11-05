916772d6b0a85c3da403bb8171d85ecef0b81c74
liquidjoo <liquidjoo@gmail.com>
2020-11-05 17:43:39 +0900

    init : project init

---
+++ src/test/java/com/github/liquidjoo/productsearch/ProductSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.liquidjoo.productsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
