256188677620f6ead2127ddd92a45e03919a61fd
liquidjoo <liquidjoo@gmail.com>
2020-11-24 18:25:02 +0900

    test : properties value test

---
+++ src/main/resources/properties-source.sql
@@ -0,0 +1,27 @@
+insert into `properties` (application, profile, label, property_key, property_value) values
+('application', 'dev', 'develop', 'security.oauth2.resource.user-info-uri', 'http://localhost/dev/develop'),
+('application', 'dev', 'develop', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'dev', 'local', 'security.oauth2.resource.user-info-uri', 'http://localhost/devl/local'),
+('application', 'dev', 'local', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'dev', 'master', 'security.oauth2.resource.user-info-uri', 'http://localhost/dev/master'),
+('application', 'dev', 'master', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'prd', 'develop', 'security.oauth2.resource.user-info-uri', 'http://localhost/prd/develop'),
+('application', 'prd', 'develop', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'prd', 'local', 'security.oauth2.resource.user-info-uri', 'http://localhost/prd/local'),
+('application', 'prd', 'local', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'prd', 'master', 'security.oauth2.resource.user-info-uri', 'http://localhost/prd/master'),
+('application', 'prd', 'master', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'local', 'develop', 'security.oauth2.resource.user-info-uri', 'http://localhost/local/develop'),
+('application', 'local', 'develop', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'local', 'local', 'security.oauth2.resource.user-info-uri', 'http://localhost/local/local'),
+('application', 'local', 'local', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
+
+('application', 'local', 'master', 'security.oauth2.resource.user-info-uri', 'http://localhost/local/master'),
+('application', 'local', 'master', 'spring.datasource.url', 'jdbc:mysql://127.0.0.1/u-payment?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Seoul'),
