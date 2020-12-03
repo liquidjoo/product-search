99b08c2ffa49d2817a95b0adc0a81e671740e839
liquidjoo <liquidjoo@gmail.com>
2020-12-03 13:30:23 +0900

    feat : properteis test

--- config-client/src/main/java/com/github/liquidjoo/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/github/liquidjoo/configclient/ConfigClientApplication.java
@@ -3,14 +3,18 @@ package com.github.liquidjoo.configclient;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class ConfigClientApplication {
@@ -26,14 +30,21 @@ public class ConfigClientApplication {
         private ApplicationConfiguration applicationConfiguration;
 //        private ContextRefresher contextRefresher;
 
+        @Value("${api.test}")
+        private String apiTest;
+
         public DefaultController(final ApplicationConfiguration applicationConfiguration) {
             this.applicationConfiguration = applicationConfiguration;
         }
 
         @GetMapping("/test")
-        public void get() {
+        public String get() {
+//            if (Objects.isNull(contextRefresher)) {
+//                contextRefresher.refresh();
+//            }
             log.info("{}", applicationConfiguration.getToken());
 
+            return apiTest;
         }
     }
 
@@ -43,7 +54,6 @@ public class ConfigClientApplication {
     @Getter
     @Setter
     public static class ApplicationConfiguration {
-
         private String token;
     }
 
